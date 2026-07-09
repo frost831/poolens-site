7d31dd7b6bccd6581aa5d0bbae5bdbbf2d1ce256
Frost <frost@throttleshare.com>
2026-07-09 11:09:39 -0500

    Preserve Facility Assist public URL

--- functions/facility-assist.js
+++ functions/facility-assist.js
@@ -1,5 +1,5 @@
 export async function onRequest({ request, env }) {
  const url = new URL(request.url);
- url.pathname = '/facility-assist-page.html';
+ url.pathname = '/facility-assist-page';
  return env.ASSETS.fetch(new Request(url, request));
 }

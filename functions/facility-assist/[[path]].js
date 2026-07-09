e312d5b0afeb58049a1d333ee33568a975600a12
Frost <frost@throttleshare.com>
2026-07-09 11:10:25 -0500

    Normalize Facility Assist trailing slash

--- functions/facility-assist/[[path]].js
+++ functions/facility-assist/[[path]].js
@@ -1,5 +1,9 @@
 export async function onRequest({ request, env }) {
  const url = new URL(request.url);
+ if (url.pathname.endsWith('/')) {
+  url.pathname = url.pathname.replace(/\/+$/, '');
+  return Response.redirect(url.toString(), 301);
+ }
  url.pathname = '/facility-assist-page';
  return env.ASSETS.fetch(new Request(url, request));
 }
